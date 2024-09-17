9f8afb82dc720a0f4131581000611393e410514a
Mikhail Dmitriev <mikdmitr@gmail.com>
2024-09-18 00:45:21 +0300

    +wcf

---
+++ IPC/WCFServer/ICalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+
+namespace WCFServer
+{
+    //[ServiceContract]
+    //public interface ICalculator
+    //{
+    //    [OperationContract]
+    //    int Add(int a, int b);
+    //}
+}
